5623915599a7cc7bfb7d66c3194570a95440e81c
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2023-07-18 18:24:20 -0300

    agregar scroll

--- build/js/app.js
+++ build/js/app.js
@@ -4,6 +4,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function iniciarApp(){
     crearGaleria();
+    scrollNav();
+}
+
+function scrollNav(){
+    const enlaces = document.querySelector('.navegacion-principal a');
+    enlaces.forEach( enlace =>{
+        enlace.addEventListener('click', function(){
+            
+        });
+    });
 }
 
 function crearGaleria(){
@@ -15,10 +25,46 @@ function crearGaleria(){
         <source srcset="build/img/thumb/${i}.avif" type="image/avif">
         <source srcset="build/img/thumb/${i}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
-        `
+        `;
 
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
         galeria.appendChild(imagen)
     }
-    
-    
+}
+
+function mostrarImagen(id){
+    const imagen = document.createElement('picture')
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+        `;
+
+    // Crea el Overlay con la imagen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    // Añadirlo al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
